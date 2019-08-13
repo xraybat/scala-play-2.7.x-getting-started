377bd5d14919461816804484da3dce7f3b6636d5
xraybat <xraybat100@gmail.com>
2019-08-13 11:04:13 +1000

    hello world parameterised message

--- app/models/HelloWorld.scala
+++ app/models/HelloWorld.scala
@@ -1,6 +1,6 @@
 package models
 
 class HelloWorld {
-  def say(str: String = "hello, world!!!!") : String = str
-  override def toString: String = "(" + say() + ")"
+  def say(str: String = "it's a great day!") : String = str
+  override def toString: String = say()
 }
