3a9b6df9453b6583814c3d1e3b094b488c461c69
xraybat <xraybat100@gmail.com>
2019-08-13 13:36:35 +1000

    static person property

--- app/models/Model.scala
+++ app/models/Model.scala
@@ -4,16 +4,13 @@ import play.api.libs.json._
 
 object Model {
   private val _helloworld = new HelloWorld
-  private val _person = new Person("fred", "nerk", 44)
+
+  val person = new Person("fred", "nerk", 44)
 
   def helloWorld(msg: String) : String = _helloworld.say(msg)
 
-  // as model properties??
   def stockGet(symbol: String, price: Double): Stock = Stock(symbol, price)
   def stockPut(json: JsValue): Unit = println(json.as[Stock])
-
-  // as model properties??
-  def person = _person
 
   def throwException = throw new MyException("myException!!")
 
